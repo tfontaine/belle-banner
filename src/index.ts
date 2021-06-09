6b9ebbd362428e17bdfbdb23dfc71d539f5980be
Thomas Fontaine <tfontaine@grove.co>
2021-06-09 05:21:39 +0000

    Relatively positioned

--- src/index.ts
+++ src/index.ts
@@ -12,30 +12,41 @@ export class BelleBanner extends LitElement {
   /**
    * Links to images to display.
    */
-  @property({ attribute: 'banner-images', type: Array })
+  @property({ attribute: 'img', type: Array })
   bannerImages = ["https://i.imgur.com/ZtdhrUA.jpg"];
 
   /**
    * Redirection links.
    */
-  @property({ attribute: 'banner-links', type: Array })
+  @property({ attribute: 'redir', type: Array })
   bannerLinks = ["https://beta.belle.ai"];
 
+  /**
+   * Initial height.
+   */
+  @property({ attribute: 'height', type: Number })
+  bannerHeight = 100;
+
+  /**
+   * Redirection links.
+   */
+  @property({ attribute: 'width', type: Array })
+  bannerWidth = 600;
+
   static styles = css`
-      button {
-        width: 4rem;
-        height: 4rem;
-        border: none;
-        border-radius: 10px;
-        background-color: seagreen;
-        color: white;
+        :host {
+					position: relative;
+					display: block;
+					overflow: visible;
+				}
       }`;
 
   render() {
     return html`
       <div>
       <p style="text-align: center;">
-      <img alt="" src="${this.bannerImages[this.sliderIndex]}" style="width: 1200px; height: 200px; border-width: 2px; border-style: solid; margin: 1px;" /></p>
+      <img alt="" src="${this.bannerImages[this.sliderIndex]}" 
+           style="width: ${this.bannerWidth}px; height: ${this.bannerHeight}px; border-width: 2px; border-style: solid; margin: 1px;" /></p>
       </div>
     `;
   }
